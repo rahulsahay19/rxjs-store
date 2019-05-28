eab16586fadbee0e3157fd50e7432756eb65773e
Rahul Sahay <rahulsahay19@hotmail.com>
2019-05-28 23:37:33 +0530

    store finished

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -6,6 +6,7 @@ import * as moment from 'moment';
 import {fromEvent} from 'rxjs';
 import {concatMap, distinctUntilChanged, exhaustMap, filter, mergeMap, tap} from 'rxjs/operators';
 import {fromPromise} from 'rxjs/internal-compatibility';
+import { Store } from '../common/store.service';
 
 @Component({
     selector: 'course-dialog',
@@ -25,7 +26,8 @@ export class CourseDialogComponent implements AfterViewInit {
     constructor(
         private fb: FormBuilder,
         private dialogRef: MatDialogRef<CourseDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) course:Course ) {
+        @Inject(MAT_DIALOG_DATA) course:Course,
+        private store: Store) {
 
         this.course = course;
 
@@ -36,6 +38,14 @@ export class CourseDialogComponent implements AfterViewInit {
             longDescription: [course.longDescription,Validators.required]
         });
 
+    }
+
+    save() {
+        this.store.saveCourse(this.course.id, this.form.value)
+            .subscribe(
+                () => this.close(),
+                err => console.log('Error Occured while saving course', err)
+            );
     }
 
     ngAfterViewInit() {
